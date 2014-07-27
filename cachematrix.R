e70e86906cdccc90aacc0dab18ce612e8b0082b8
Kyle Baird <kyle.baird@milliman.com>
2014-07-26 20:25:36 -0400

    Add comments
    Further expand the documentation in the code.

--- cachematrix.R
+++ cachematrix.R
@@ -8,13 +8,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
     mat.inv <- NULL
+    ## Set the value of the matrix
     set <- function(y) {
         x <<- y
         mat.inv <<- NULL
     }
+    ## Get the value of the matrix
     get <- function() x
+    ## Set the value of the inverse
     setsolve <- function(solve) mat.inv <<- solve
+    ## Get the value of the inverse
     getsolve <- function() mat.inv
+    ## Return the matrix and the functions
     list(set = set, get = get,
          setsolve = setsolve,
          getsolve = getsolve)
@@ -28,12 +33,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     mat.inv <- x$getsolve()
+    ## If getsolve does not return NULL, it means
+    ## we have already have the value.  So, return
+    ## it.
     if(!is.null(mat.inv)) {
         message("getting cached data")
         return(mat.inv)
     }
+    ## Otherwise calculate it
     data <- x$get()
     mat.inv <- solve(data, ...)
+    ## Update our cache to store it
     x$setsolve(mat.inv)
+    ## return it to the user
     mat.inv
 }
